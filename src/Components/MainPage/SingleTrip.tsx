5852101b1df1e8f1733ba03501883e33aefaefd5
Xuan Ng <118996751+yeexng@users.noreply.github.com>
2023-05-03 01:08:15 +0100

    Single trip page done

--- src/Components/MainPage/SingleTrip.tsx
+++ src/Components/MainPage/SingleTrip.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./SingleTrip.css";
+import "./ChatBubble.scss";
 import {
   Col,
   Form,
@@ -9,6 +10,7 @@ import {
   Row,
   InputGroup,
   Figure,
+  Button,
 } from "react-bootstrap";
 
 const SingleTrip = () => {
@@ -68,7 +70,7 @@ const SingleTrip = () => {
         </Navbar>
       </div>
       <div style={{ height: "94vh" }}>
-        <div className="single-secret-layout">
+        <div className="single-trip-layout">
           <Row className="main-row-layout">
             <Col md={8}>
               <div className="trip-single-div d-flex justify-content-center align-items-center mx-5">
@@ -101,92 +103,65 @@ const SingleTrip = () => {
                       quod eius illum, ab nam doloremque possimus laudantium
                       voluptatem a.
                     </p>
+                    <Button variant="outline-danger">Connect</Button>
                   </Col>
                 </Row>
               </div>
             </Col>
-            {/* <Col className="post-text m-0">
+            <Col className="post-text m-0">
               <div className="mt-4">
                 <p>
                   <img
                     className="profile-img mr-3"
                     src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
                   ></img>
-                  Author
-                </p>
-                <h3>Title: Time to test this page</h3>
-                <p className="mb-0">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus beatae sit exercitationem tempore voluptates. Optio
-                  adipisci nemo dolore ducimus voluptate quo at error.
-                  Laboriosam cum perferendis consequatur voluptatibus quia
-                  expedita?Lorem ipsum dolor sit amet consectetur, adipisicing
-                  elit. Iure dignissimos earum dolor modi enim debitis autem
-                  dolores. Natus commodi, error vel voluptas nesciunt
-                  perspiciatis deleniti vitae porro? Nam, neque magnam!
-                </p>
-                <p className="mt-2 post-date">Post time</p>
-              </div>
-              <hr></hr>
-              <div>
-                <p>90 Comments</p>
-                <p>
-                  <Row>
-                    <Col md={1}>
-                      <img
-                        className="profile-img mr-3"
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                      ></img>
-                    </Col>
-                    <Col>
-                      <InputGroup>
-                        <Form.Control placeholder="Show your appreciation here..." />
-                      </InputGroup>
-                    </Col>
-                  </Row>
+                  Sam Ng
                 </p>
               </div>
-              <div>
-                <div className="single-comment mt-4">
-                  <Row>
-                    <Col md={1} className="pr-0">
-                      <img
-                        className="profile-img"
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                      ></img>
-                    </Col>
-                    <Col className="ml-1">
-                      <p>Sam Ng</p>
-                      <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Vel adipisci in natus reiciendis rem pariatur
-                        voluptatem
+              <div className="chat-history mb-3">
+                {/* Chat Bubble */}
+                <div className="speech-wrapper">
+                  <div className="bubble">
+                    <div className="txt">
+                      <p className="name">Sam Ng</p>
+                      <p className="message">
+                        Hey, check out this Pure CSS speech bubble...
                       </p>
-                      <p className="mt-2 post-date">1 Nov 2022</p>
-                    </Col>
-                  </Row>
-                </div>
-                <div className="single-comment mt-4">
-                  <Row>
-                    <Col md={1} className="pr-0">
-                      <img
-                        className="profile-img"
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                      ></img>
-                    </Col>
-                    <Col className="ml-1">
-                      <p>Sam Ng</p>
-                      <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Vel adipisci in natus reiciendis rem pariatur
-                        voluptatem
+                      <span className="timestamp">10:20 pm</span>
+                    </div>
+                    <div className="bubble-arrow"></div>
+                  </div>
+                  {/* Speech Bubble alternative */}
+                  <div className="bubble alt mr-2">
+                    <div className="txt">
+                      <p className="name alt">
+                        You<span> ~ Xuan Ng</span>
                       </p>
-                      <p className="mt-2 post-date">1 Nov 2022</p>
-                    </Col>
-                  </Row>
+                      <p className="message">
+                        Nice... this will work great for my new project.
+                      </p>
+                      <span className="timestamp">10:22 pm</span>
+                    </div>
+                    <div className="bubble-arrow alt"></div>
+                  </div>
                 </div>
               </div>
-            </Col> */}
+              <div>
+                <Row className="chat-input mt-2">
+                  <Col md={1}>
+                    <img
+                      className="profile-img mr-3"
+                      src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                    ></img>
+                  </Col>
+                  <Col>
+                    <InputGroup>
+                      <Form.Control placeholder="Show your appreciation here..." />
+                    </InputGroup>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
           </Row>
         </div>
       </div>
